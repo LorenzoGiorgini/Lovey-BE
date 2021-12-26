857fe81bc17e8dbcf94439f0342ca383ec5cbef5
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-12-26 12:09:15 +0100

    add error handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import express from 'express';
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import expressListEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
+
+import {
+  unauthorizedHandler,
+  forbiddenHandler,
+  catchAllHandler,
+} from "./errorHandlers/index.js";
 
 const app = express();
 const port = process.env.PORT || 3030;
@@ -10,22 +16,21 @@ const port = process.env.PORT || 3030;
 app.use(express.json());
 app.use(cors());
 
-
 //Routes
-
-
-
 
 //Error handlers
 
+app.use(unauthorizedHandler)
+app.use(forbiddenHandler)
+app.use(catchAllHandler)
 
 
-mongoose.connect(process.env.MONGO_CONNECTION)
+mongoose.connect(process.env.MONGO_CONNECTION);
 
-mongoose.connection.on('connected', () => {
-    console.log('MongoDB connected')
-    app.listen(port, () => {
-        constole.table(expressListEndpoints(app))
-        console.log("server on port" + port)
-    })
-})
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected");
+  app.listen(port, () => {
+    console.table(expressListEndpoints(app));
+    console.log("server on port " + port);
+  });
+});
